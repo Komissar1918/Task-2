6efb78f57a5839674a442bc2568386aa58787349
Yosyf Akhmetov <Avtograd-Rabota2020@yandex.ru>
2023-02-17 14:06:18 +0300

    TaskCompleted

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,16 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean = (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean2 = (HelloWorld) applicationContext.getBean("helloworld");
+
+        Cat binCat = (Cat) applicationContext.getBean("cat");
+        Cat binCat2 = (Cat) applicationContext.getBean("cat");
+
         System.out.println(bean.getMessage());
+
+        System.out.println(bean == bean2);
+        System.out.println(binCat == binCat2);
+
     }
 }
